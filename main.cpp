0a2895be3059d62017308329c4ee712686850aba
werde <dolfaq@gmail.com>
2019-04-01 12:24:18 +0300

    hui

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include <curl/curl.h>
+
+int main()
+{
+    std::stringstream str;
+
+    auto easyhandle = curl_easy_init();
+
+
+    return 0;
+}
